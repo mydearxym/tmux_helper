e1e2eeecdb7434ec87224ba6f4487a88f67bef2f
mydearxym <mydearxym@qq.com>
2014-06-15 23:35:29 +0800

    add tmux.conf config

--- t_helper.sh
+++ t_helper.sh
@@ -4,6 +4,32 @@
 # license : GPL
 # created : 2014-06-15
 
+# set-option -g prefix C-j
+# unbind-key C-b
+# bind-key C-j send-prefix
+# #setw -g mode-key vi
+# setw -g mode-mouse on
+# set -g mouse-select-pane on
+# set -g mouse-resize-pane on
+# set -g mouse-select-window on
+
+# bind-key 3 split-window -h
+# bind-key 2 split-window
+# bind-key k selectp -U
+# bind-key j selectp -D
+# bind-key k selectp -L
+# bind-key l selectp -R
+
+# set-option -g status on
+# set-option -g status-interval 2
+# set-option -g status-utf8 on
+# set-option -g status-justify "centre"
+# set-option -g status-left-length 60
+# set-option -g status-right-length 90
+# set-option -g status-left "#(~/code/tmux-powerline/powerline.sh left)"
+
+# set-window-option -g window-status-current-format "#[fg=colour235, bg=colour27]⮀#[fg=colour255, bg=colour27] #I []#W #[fg=colour27, bg=colour235][]
+
 
 cmd=$(which tmux)
 session=rails
